d8fe2b329cd58d69f7d7063b22ec1d0a4fb2df34
Machavane <150149062+Machavane@users.noreply.github.com>
2024-01-03 17:18:54 +0300

    V0.06

--- styles/js/main.js
+++ styles/js/main.js
@@ -4,3 +4,8 @@ $(function () {
         $(this).toggleClass('menu-mobile-limit_active');
     });
 });
+
+window.addEventListener("scroll", function (){
+    let header = document.querySelector('.header-top')
+    header.classList.toggle('rolagem', window.scrollY > 0)
+})
