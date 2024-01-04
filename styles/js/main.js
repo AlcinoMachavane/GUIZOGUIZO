fc49b5dbe63d0e248118f50f260bc62c87d0d870
Machavane <150149062+Machavane@users.noreply.github.com>
2024-01-04 19:58:33 +0300

    V0.07

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,16 +1,11 @@
 $(function () {
-    $('.menu-mobile-limit').on('click', function () {
+    $('.menu-mobile-limit').on('click', function(){
         $('.menu-mobile-list').toggleClass('ds_none');
         $(this).toggleClass('menu-mobile-limit_active');
     });
 });
 
-// window.addEventListener("scroll", function () {
-//     let header = document.querySelector('.header-top, .menu-mobile-list')
-//     header.classList.toggle('rolagem', window.scrollY > 0);
-// })
-window.addEventListener("touchstart", function () {
+window.addEventListener("scroll", function (){
     let header = document.querySelector('.header-top, .menu-mobile-list')
     header.classList.toggle('rolagem', window.scrollY > 0)
-
 })
